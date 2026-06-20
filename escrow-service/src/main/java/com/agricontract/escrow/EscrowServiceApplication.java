e74d56852b6b11eb56abf8c889e64d7651986b48
RedAvocado22 <minhcuong2922004@gmail.com>
2026-06-20 18:46:38 +0700

    feat(escrow-svc): outbox table + poller publishing PENDING domain events
    escrow_domain_events outbox table + OutboxPoller, written in same DB transaction as the aggregate.

--- escrow-service/src/main/java/com/agricontract/escrow/EscrowServiceApplication.java
+++ escrow-service/src/main/java/com/agricontract/escrow/EscrowServiceApplication.java
@@ -2,7 +2,9 @@ package com.agricontract.escrow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EscrowServiceApplication {
     public static void main(String[] args) {
